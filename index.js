dffad40076da8e3f1147fd54fd7cd96c685cd00f
mike <mike@fusebuilder.net>
2016-06-08 23:02:59 -0700

    updates to package

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ app.get('/register/:url', function(req, resp){
 		}
 		return resp.end(JSON.stringify({
 			'original': result.url,
-			'shortened': process.env.APP_DOMAIN+result.hash}));
+			'shortened': process.env.APP_DOMAIN
+					+((process.env.APP_PORT != '80') ? ':'+process.env.APP_PORT+'/' : '/')
+					+result.hash}));
 	});
 	
 });
